70ad95cb1f58f933749eaf8dc6e50c4dbcc94f29
Daniela Serra <daniserra.it@gmail.com>
2023-01-30 12:08:43 -0300

    2nd commit: exercises 4-5

--- Clase04-While/Source.cpp
+++ Clase04-While/Source.cpp
@@ -7,8 +7,17 @@ using namespace std;
 
 int main()
 {
-	//variables acatividades 1, 2, 3
+	//variables acatividades 1, 2, 3, 5
 	int x;
+
+	//Variable Contador
+	int c;
+
+	//variable actividad 4
+	int valPos;
+
+	//variable actividad 5
+	int suma, promedio;
 
 	/*
 	//Actividad 1
@@ -39,7 +48,7 @@ int main()
 			<< ", ";
 		x = x + 1;
 	}
-	*/
+	
 
 	//Actividad 3
 	//Imprimir los números del 2 al 100 pero de 2 en 2 (2,4,6,8 ....100).
@@ -55,5 +64,61 @@ int main()
 		x += 2;
 	}
 
+	
+
+	//Actividad 4
+	//Escribir un programa que solicite la carga de un valor positivo y nos muestre desde 1 hasta el valor ingresado de uno en uno.
+
+	cout
+		<< "Ingrese un valor positivo: ";
+	cin
+		>> valPos;
+
+	c = 1;
+
+	while (valPos >= c)
+	{
+		cout
+			<< c;
+		cout
+			<< " - ";
+
+		c += 1;
+	}
+	*/
+
+	//Actividad 5
+	//Desarrollar un programa que permita la carga de 10 valores por teclado y nos muestre posteriormente la suma de los valores ingresados y su promedio.
+
+	suma = 0;
+	c = 1;
+
+	while (c <= 10)
+	{
+		cout
+			<< "Escriba un valor: ";
+		cin
+			>> x;
+
+		suma += x;
+		c += 1;
+	}
+
+	promedio = suma / 10;
+
+	cout
+		<< "La suma de sus valores es: ";
+	cout
+		<< suma;
+	cout
+		<< "\n";
+	cout
+		<< "El promedio de sus valores es: ";
+	cout
+		<< promedio;
+
+		
+
 	return 0;
+	
 }
