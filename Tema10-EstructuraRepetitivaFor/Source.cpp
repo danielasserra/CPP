2d96d55a1316ef67489a4b84b5b4026fd4a7544d
Daniela Serra <daniserra.it@gmail.com>
2023-01-31 16:45:17 -0300

    4th commit. All exercises completed

--- Tema10-EstructuraRepetitivaFor/Source.cpp
+++ Tema10-EstructuraRepetitivaFor/Source.cpp
@@ -255,7 +255,390 @@ int main()
 	}
 	cout
 		<< suma;
-		*/
+		
+
+	//Actividad 3
+	//Desarrollar un programa que muestre la tabla de multiplicar del 5 (del 5 al 50)
+
+	int f;
+	int x=0;
+
+	for (f = 0; f < 10; f++)
+	{
+		x = x + 5;
+
+		cout
+			<< x;
+		cout
+			<< ", ";
+	}
+	
+	
+	//Confeccionar un programa que permita ingresar un valor del 1 al 10 y nos muestre la tabla de multiplicar del mismo (los primeros 12 términos)
+	//Ejemplo: Si ingreso 3 deberá aparecer en pantalla los valores 3, 6, 9, hasta el 36.
+
+	int x;
+	int f; //contador
+
+	cout
+		<< "ingrese un numero del 1 al 10: ";
+	cin
+		>> x;
+
+	for (f = x; f <= x * 12; f += x)
+	{
+		cout
+			<< f;
+		cout
+			<< ", ";
+	}
+
+	
+	//Actividad 5
+	//Realizar un programa que lea los lados de n triángulos, e informar:
+	//a) De cada uno de ellos, qué tipo de triángulo es : equilátero(tres lados iguales), isósceles(dos lados iguales), o escaleno(ningún lado igual)
+	//b) Cantidad de triángulos de cada tipo.
+	//c) Tipo de triángulo que posee menor cantidad.
+	
+	int triangulos; // ingreso por usuario
+	int ladoUno = 0, ladoDos = 0, ladoTres = 0; //tamaño lados
+	int equilatero = 0; 
+	int isosceles = 0; 
+	int escaleno = 0;
+	int f;
+
+	cout
+		<< "Ingresar la cantidad de triangulos: ";
+	cin
+		>> triangulos;
+
+	for (f = 0; f < triangulos; f++)
+	{
+		cout
+			<< "Ingresar tres medidas, correspondientes a los lados del triangulo: ";
+		cout
+			<< "\n";
+		cin
+			>> ladoUno;
+		cin
+			>> ladoDos;
+		cin
+			>> ladoTres;
+		if (ladoUno == ladoDos && ladoUno == ladoTres)
+		{
+			equilatero += 1;
+
+			cout
+				<< "Es un triangulo equilatero";
+			cout
+				<< "\n";
+		}
+		else
+		{
+			if (ladoUno == ladoDos || ladoUno == ladoTres || ladoDos == ladoTres)
+			{
+				isosceles += 1;
+
+				cout
+					<< "Es un triangulo Isosceles";
+				cout
+					<< "\n";
+			}
+			else
+			{
+				escaleno += 1;
+
+				cout
+					<< "Es un triangulo Escaleno";
+				cout
+					<< "\n";
+			}
+		}
+		
+	}
+	cout
+		<< "Cantidad de triangulos equilateros: ";
+	cout
+		<< equilatero;
+	cout
+		<< "\n";
+	cout
+		<< "Cantidad de triangulos isosceles: ";
+	cout
+		<< isosceles;
+	cout
+		<< "\n";
+	cout
+		<< "Cantidad de triangulos escalenos: ";
+	cout
+		<< escaleno;
+	cout
+		<< "\n";
+
+	if (equilatero < isosceles && equilatero < escaleno)
+	{
+		cout
+			<< "Hay menor cantidad de triangulos equilateros";
+	}
+	else
+	{
+		if (isosceles < equilatero && isosceles < escaleno)
+		{
+			cout
+				<< "Hay menor cantidad de triangulos isosceles";
+		}
+		else
+		{
+			cout
+				<< "Hay menor cantidad de triangulos escalenos";
+		}
+	}
+	
+	//Actividad 6
+	//Escribir un programa que pida ingresar coordenadas (x,y) que representan puntos en el plano.
+	//Informar cuántos puntos se han ingresado en el primer, segundo, tercer y cuarto cuadrante.Al comenzar el programa se pide que se ingrese la cantidad de puntos a procesar.
+
+	int puntos; //cantidad de puntos a procesar
+	int x, y;
+	int primerCuad = 0, segundoCuad = 0, tercerCuad = 0, cuartoCuad = 0;
+	int f;
+
+	cout
+		<< "Ingrese la cantidad de puntos a procesar: ";
+	cin
+		>> puntos;
+
+	for (f = 0; f < puntos; f++)
+	{
+		cout
+			<< "Ingrese coordenada x: ";
+		cin
+			>> x;
+		cout
+			<< "Ingrese coordenada y: ";
+		cin
+			>> y;
+		cout
+			<< "\n";
+
+		if (x > 0 && y > 0)
+		{
+			primerCuad += 1;
+		}
+		else
+		{
+			if (x < 0 && y > 0)
+			{
+				segundoCuad += 1;
+			}
+			else
+			{
+				if (x < 0 && y < 0)
+				{
+					tercerCuad += 0;
+				}
+				else
+				{
+					cuartoCuad += 1;
+				}
+			}
+		}
+
+	}
+	cout
+		<< "En el primer cuadrante se han ingresado ";
+	cout
+		<< primerCuad;
+	cout
+		<< "puntos";
+	cout
+		<< "\n";
+	cout
+		<< "En el segundo cuadrante se han ingresado ";
+	cout
+		<< segundoCuad;
+	cout
+		<< "puntos";
+	cout
+		<< "\n";
+	cout
+		<< "En el tercer cuadrante se han ingresado ";
+	cout
+		<< tercerCuad;
+	cout
+		<< "puntos";
+	cout
+		<< "\n";
+	cout
+		<< "En el cuarto cuadrante se han ingresado ";
+	cout
+		<< cuartoCuad;
+	cout
+		<< "puntos";
+	cout
+		<< "\n";
+		
+
+
+	//Actividad7
+	//Se realiza la carga de 10 valores enteros por teclado.Se desea conocer :
+	//a) La cantidad de valores ingresados negativos.
+	//b) La cantidad de valores ingresados positivos.
+	//c) La cantidad de múltiplos de 15.
+	//d) El valor acumulado de los números ingresados que son pares.
+	int f;
+	int valor;
+	int neg=0;
+	int pos=0;
+	int quince=0;
+	int par = 0;
+
+	for (f = 0; f < 10; f++)
+	{
+		cout
+			<< "Ingrese un numero entero: ";
+		cin
+			>> valor;
+
+		if (valor < 0)
+		{
+			neg++;
+		}
+		else
+		{
+			if (valor > 0)
+			{
+				pos++;
+			}
+		}
+
+		if (valor % 15 == 0)
+		{
+			quince++;
+		}
+		if (valor % 2 == 0)
+		{
+			par++;
+		}
+	}
+
+	cout
+		<< "Valores negativos ingresados: ";
+	cout
+		<< neg;
+	cout
+		<< "\n";
+	cout
+		<< "Valores positivos ingresados: ";
+	cout
+		<< pos;
+	cout
+		<< "\n";
+	cout
+		<< "Cantidad de multiplos de quince: ";
+	cout
+		<< quince;
+	cout
+		<< "\n";
+	cout
+		<< "cantidad de multiplos de dos: ";
+	cout
+		<< par;
+
+	*/
+	/*Actividad 8
+	Se cuenta con la siguiente información:
+Las edades de 5 estudiantes del turno mañana.
+Las edades de 6 estudiantes del turno tarde.
+Las edades de 11 estudiantes del turno noche.
+Las edades de cada estudiante deben ingresarse por teclado.
+a) Obtener el promedio de las edades de cada turno (tres promedios)
+b) Imprimir dichos promedios (promedio de cada turno)
+c) Mostrar por pantalla un mensaje que indique cual de los tres turnos tiene un promedio de edades mayor.*/	
+
+	int f;
+	int tm;
+	int sumaTm=0;
+	int promedioTm;
+	int tt;
+	int sumaTt=0;
+	int promedioTt;
+	int tn;
+	int sumaTn=0;
+	int promedioTn;
+
+	for (f = 1; f <= 5; f++)
+	{
+		cout
+			<< "Ingrese edad de estudiante TM: ";
+		cin
+			>> tm;
+
+		sumaTm += tm;
+
+	}
+	for (f = 1; f <= 6; f++)
+	{
+		cout
+			<< "Ingrese edad de estudiante TT: ";
+		cin
+			>> tt;
+
+		sumaTt += tt;
+	}
+
+	for (f = 1; f <= 11; f++)
+	{
+		cout
+			<< "Ingrese edad de estudiante TN: ";
+		cin
+			>> tn;
+
+		sumaTn++;
+	}
+
+	promedioTm = sumaTm / 5;
+	cout
+		<< "La edad promedio de los alumnos del TM es de: ";
+	cout
+		<< promedioTm;
+	cout
+		<< "\n";
+
+	promedioTt = sumaTt / 6;
+	cout
+		<< "La edad promedio de los alumnos del TT es de: ";
+	cout
+		<< promedioTt;
+	cout
+		<< "\n";
+
+	promedioTn = sumaTn / 11;
+	cout
+		<< "La edad promedio de los alumnos del TN es de: ";
+	cout
+		<< promedioTn;
+	cout
+		<< "\n";
+
+	if (promedioTm > promedioTt && promedioTm > promedioTn)
+	{
+		cout
+			<< "El TM tiene un promedio de edades mayor";
+	}
+	else
+	{
+		if (promedioTt > promedioTn)
+		{
+			cout
+				<< "El TT tiene un promedio de edades mayor";
+		}
+		else
+		{
+			cout
+				<< "El TN tiene un promedio de edades mayor";
+		}
+	}
 
 
 	return 0;
