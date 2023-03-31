e832d5ff3f7bac9d9b6c355eb26f31ade26e0ac9
Daniela Serra <daniserra.it@gmail.com>
2023-03-31 10:52:07 -0300

    ejercicios completos

--- Tema15-EstructuraDatosTipoVector/Source.cpp
+++ Tema15-EstructuraDatosTipoVector/Source.cpp
@@ -439,9 +439,44 @@ private:
     int vec[10];
 public:
     void cargar();
-    void menMay();
+    void ordenado();
 };
 
 void pruebaVector7::cargar() {
+    cout << "Cargar 10 elementos.";
+    cout << "\n";
 
+    for (int f = 0; f < 10; f++) {
+        cout
+            << "Ingrese un numero: ";
+        cin >> vec[f];
+    }
+
+}
+
+void pruebaVector7::ordenado() {
+    
+    int orden = 1;
+
+    for (int f = 0; f < 9; f++) {
+
+        if (vec[f + 1] < vec[f]) {
+            orden = 0;
+        }
+    }
+    if (orden == 1) {
+        cout << "Esta ordenado de menor a mayor";
+    }
+    else {
+        cout << "No esta ordenado de menor a mayor";
+    }
+    cin.get();
+    cin.get();
+}
+
+int main() {
+    pruebaVector7 pv7;
+    pv7.cargar();
+    pv7.ordenado();
+    return 0;
 }
